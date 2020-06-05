9fc0e5e2803d854a90d66550e3d0268a2ae283fe
Finn Innes <finn123890@gmail.com>
2020-06-05 14:14:19 +1200

    COMPX341-20 A4 VERSION 2 FINAL

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -15,7 +15,7 @@ exports.getWeather = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',nz';
+	var aurl = OPENWEATHERURL;
 
 	request({
 		method: 'GET',
@@ -45,7 +45,7 @@ exports.getWeather3 = function(req, res) {
 		return res.status(400).send('zip missing');
 	}
 
-	var aurl = OPENWEATHERURL + '&zip=' + zip + ',us';
+	var aurl = OPENWEATHERURL;
 
 	request({
 		method: 'GET',
